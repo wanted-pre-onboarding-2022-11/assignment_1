3bd00878628b5053603432d2cb09b726a2ea9cf0
yong <tkddbs1644@gmail.com>
2022-10-27 17:39:57 +0900

    Feat: api 모듈 추가

---
+++ src/apis/index.ts
@@ -0,0 +1,67 @@
+import { getAccessToken } from "@/utils/localStorage";
+import axios, { AxiosRequestConfig } from "axios";
+
+const { API_END_POINT } = process.env;
+
+interface RequestBody {
+  email: string;
+  password: string;
+}
+
+interface ITodo {
+  id: number;
+  todo: string;
+  isCompleted: boolean;
+  userId: number;
+}
+
+export const api = axios.create({
+  // baseURL: process.env.REACT_APP_URL,
+  baseURL: API_END_POINT as string,
+  timeout: 2000,
+});
+
+api.interceptors.request.use(
+  (config: AxiosRequestConfig) => {
+    const token = getAccessToken();
+    const newConfig = { ...config };
+    if (newConfig.headers && token) {
+      newConfig.headers.Authorization = `Bearer ${token}`;
+    }
+    return newConfig;
+  },
+  (error) => Promise.reject(error.response),
+);
+
+export const accountAPI = {
+  postLogin: async (data: RequestBody): Promise<{ access_token: string }> => {
+    const response = await api.post("auth/signin", data);
+    return response.data;
+  },
+  postSignUp: async (data: RequestBody): Promise<{ access_token: string }> => {
+    const response = await api.post("auth/signup", data);
+    return response.data;
+  },
+};
+
+export const todoAPI = {
+  getTodos: async (): Promise<ITodo[]> => {
+    const response = await api.get("todos");
+    return response.data;
+  },
+  addTodo: async (todo: string): Promise<ITodo> => {
+    const response = await api.post("todos", todo);
+    return response.data;
+  },
+  deleteTodo: async (id: number) => {
+    const response = await api.delete(`todos/${id}`);
+    return response;
+  },
+  upDateTodo: async (
+    id: number,
+    { todo, isCompleted }: { todo: string; isCompleted: boolean },
+  ): Promise<ITodo> => {
+    const response = await api.put(`todos/${id}`, { todo, isCompleted });
+    return response.data;
+  },
+};
